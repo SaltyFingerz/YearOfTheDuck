99d23c57498ce094241a9e62f1d2126cda36b46e
CyborgCabbage <finlaylaidlaw9@gmail.com>
2022-10-09 16:26:35 +0100

    Level1 and dead body fix

--- Assets/Prefabs/Dead.cs
+++ Assets/Prefabs/Dead.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Dead : MonoBehaviour
 {
-    [SerializeField] private bool triggerActive = false;
-    bool toggle;
-    bool carrying = true;
+    public bool triggerActive = false;
+    public bool toggle;
+    public bool carrying = false;
     public GameObject Player;
-    BoxCollider2D boxC2D;
+    public BoxCollider2D boxC2D;
+    public Rigidbody2D rb;
 
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         boxC2D = gameObject.GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -40,15 +42,15 @@ public class Dead : MonoBehaviour
             if (triggerActive && Input.GetKeyDown(KeyCode.E))
             {
                 toggle = !toggle;
-                carrying = !carrying;
             }
-            if (toggle == true && carrying == false)
+            if (toggle)
             {
                 Drag();
             }
             else
             {
-                boxC2D.enabled = true;
+                gameObject.layer = LayerMask.NameToLayer("Default");
+                //boxC2D.enabled = true;
             }
         }
         else
@@ -60,7 +62,11 @@ public class Dead : MonoBehaviour
 
     public void Drag()
     {
-        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position + Vector3.up * 2 - Mathf.Sign(Player.transform.localScale.x)*Vector3.right + new Vector3(0.31f,0.31f,0), Time.time);
-        boxC2D.enabled = false;
+
+        
+        Vector3 temp = Vector3.MoveTowards(transform.position, Player.transform.position + Vector3.up * 1 - Mathf.Sign(Player.transform.localScale.x)*Vector3.right + new Vector3(0.31f,0.31f,0), 0.1f);
+        rb.MovePosition(new Vector2(temp.x, temp.y));
+        gameObject.layer = LayerMask.NameToLayer("Carried Body");
+        //boxC2D.enabled = false;
     }
 }
